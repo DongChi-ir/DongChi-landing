c905a9371c42313de15736f6d79ffe4b62b9d2f1
amirhossein <shamsiamir2000@gmail.com>
2024-08-03 23:15:07 +0330

    making landing

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -16,8 +16,8 @@ export default function HeroHome() {
         <div className="pb-12 pt-32 md:pb-20 md:pt-40">
           {/* Section header */}
           <div className="pb-12 text-center md:pb-16">
-            <div
-              className="mb-6 border-y [border-image:linear-gradient(to_right,transparent,theme(colors.slate.300/.8),transparent)1]"
+            {/*<div
+              className="mb-6"
               data-aos="zoom-y-out"
             >
               <div className="-mx-0.5 flex justify-center -space-x-3">
@@ -64,9 +64,9 @@ export default function HeroHome() {
                   alt="Avatar 05"
                 />
               </div>
-            </div>
+            </div>*/}
             <h1
-              className="mb-6 border-y text-5xl font-bold [border-image:linear-gradient(to_right,transparent,theme(colors.slate.300/.8),transparent)1] md:text-6xl"
+              className="mb-6 text-5xl font-bold md:text-6xl"
               data-aos="zoom-y-out"
               data-aos-delay={150}
             >
@@ -82,7 +82,7 @@ export default function HeroHome() {
                 Simple is a modern website builder powered by AI that changes
                 how companies create user interfaces together.
               </p>
-              <div className="relative before:absolute before:inset-0 before:border-y before:[border-image:linear-gradient(to_right,transparent,theme(colors.slate.300/.8),transparent)1]">
+              <div className="relative">
                 <div
                   className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center"
                   data-aos="zoom-y-out"
@@ -115,7 +115,7 @@ export default function HeroHome() {
             data-aos="zoom-y-out"
             data-aos-delay={600}
           >
-            <div className="relative aspect-video rounded-2xl bg-gray-900 px-5 py-3 shadow-xl before:pointer-events-none before:absolute before:-inset-5 before:border-y before:[border-image:linear-gradient(to_right,transparent,theme(colors.slate.300/.8),transparent)1] after:absolute after:-inset-5 after:-z-10 after:border-x after:[border-image:linear-gradient(to_bottom,transparent,theme(colors.slate.300/.8),transparent)1]">
+            {/*<div className="relative aspect-video rounded-2xl bg-gray-900 px-5 py-3 shadow-xl">
               <div className="relative mb-8 flex items-center justify-between before:block before:h-[9px] before:w-[41px] before:bg-[length:16px_9px] before:[background-image:radial-gradient(circle_at_4.5px_4.5px,_theme(colors.gray.600)_4.5px,_transparent_0)] after:w-[41px]">
                 <span className="text-[13px] font-medium text-white">
                   cruip.com
@@ -145,7 +145,7 @@ export default function HeroHome() {
                   Package published.
                 </span>
               </div>
-            </div>
+            </div>*/}
           </div>
         </div>
       </div>
